acca8593a70be0043e1efc441a6f9a4b429fd845
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-04-23 15:40:17 +0300

    Add Footer File

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2d8c90189bdcd079"; }
+/******/ 		__webpack_require__.h = function() { return "1deaeb5ba63080e2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,24 @@
+import { UilGithub, UilLinkedin, UilInstagram } from '@iconscout/react-unicons';
+import css from '../styles/Footer.module.css';
+import Image from 'next/image';
+import Logo from '../public/Logo.png';
+
 export default function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+       
+           <div className={css.container}>
+            <span>Copyright &copy; 2024</span>
+            <div className={css.social}>
+                <UilGithub size={45} />
+                <UilLinkedin size={45}/>
+                <UilInstagram size={45}/>
+            </div>
+
+            <div className={css.logo}>
+                <Image src={Logo} alt="" width={50} height={50} />
+                <span>FD</span>
+            </div>
+           </div>
+        
     )
 };
