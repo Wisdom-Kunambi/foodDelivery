9076458e738e73f676a2ae8ae3d9e0b5168ebe9b
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-04-23 14:22:07 +0300

    Second Commit and Add Header File

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bb41a1951ed6230a"; }
+/******/ 		__webpack_require__.h = function() { return "2d8c90189bdcd079"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -176,6 +176,7 @@
 /******/ 					script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 				}
 /******/ 				script.setAttribute("data-webpack", dataWebpackPrefix + key);
+/******/ 		
 /******/ 				script.src = __webpack_require__.tu(url);
 /******/ 			}
 /******/ 			inProgress[url] = [done];
@@ -189,7 +190,6 @@
 /******/ 				doneFns && doneFns.forEach(function(fn) { return fn(event); });
 /******/ 				if(prev) return prev(event);
 /******/ 			}
-/******/ 			;
 /******/ 			var timeout = setTimeout(onScriptComplete.bind(null, undefined, { type: 'timeout', target: script }), 120000);
 /******/ 			script.onerror = onScriptComplete.bind(null, script.onerror);
 /******/ 			script.onload = onScriptComplete.bind(null, script.onload);
@@ -215,11 +215,6 @@
 /******/ 			if (!module.children) module.children = [];
 /******/ 			return module;
 /******/ 		};
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/runtimeId */
-/******/ 	!function() {
-/******/ 		__webpack_require__.j = "webpack";
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/trusted types policy */
@@ -271,7 +266,6 @@
 /******/ 		var currentUpdateApplyHandlers;
 /******/ 		var queuedInvalidatedModules;
 /******/ 		
-/******/ 		// eslint-disable-next-line no-unused-vars
 /******/ 		__webpack_require__.hmrD = currentModuleData;
 /******/ 		
 /******/ 		__webpack_require__.i.push(function (options) {
@@ -332,8 +326,8 @@
 /******/ 					Object.defineProperty(fn, name, createPropertyDescriptor(name));
 /******/ 				}
 /******/ 			}
-/******/ 			fn.e = function (chunkId) {
-/******/ 				return trackBlockingPromise(require.e(chunkId));
+/******/ 			fn.e = function (chunkId, fetchPriority) {
+/******/ 				return trackBlockingPromise(require.e(chunkId, fetchPriority));
 /******/ 			};
 /******/ 			return fn;
 /******/ 		}
@@ -525,8 +519,7 @@
 /******/ 									updatedModules
 /******/ 								);
 /******/ 								return promises;
-/******/ 							},
-/******/ 							[])
+/******/ 							}, [])
 /******/ 						).then(function () {
 /******/ 							return waitForBlockingPromises(function () {
 /******/ 								if (applyOnUpdate) {
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,30 @@
+import css from '../styles/Header.module.css';
+import Image from 'next/image';
+import Logo from '../public/Logo.png';
+import { UilShoppingBag } from '@iconscout/react-unicons';
 export default function Header() {
     return (
-        <div>
-            Header
+        <div className={css.header}>
+            {/* Logo Side */}
+            <div className={css.logo}>
+                <Image src={Logo} alt="" width={50} height={50} />
+                <span>FD</span>
+            </div>
+
+            {/* Menu Side */}
+            <ul className={css.menu}>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Contact</li>
+            </ul>
+
+            {/* Right Side */}
+            <div className={css.rightSide}>
+                <div className={css.cart}>
+                    <UilShoppingBag size={35} color="#2E2E2E" />
+                    <div className={css.badge}>1</div>
+                </div>
+            </div>
         </div>
     )
 };
